cbeab74110cbd49a2fdf00f8d628b15ec0053c57
Javohir Annakulov <joe@MacBook-Air-Javohir.local>
2025-07-15 05:54:47 +0500

    finish commit

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 
 const p = document.querySelectorAll('p');
 console.log(p);
+
+// const script = document.createElement('script');
+// script.src = 'test.js';
+// document.body.append(script); // этот код доказывает что по дефолту все работает асинхронно
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+
+loadScript('test.js');
+loadScript('some.js');
+
+// изза того что бы в функции добавили async = false функции будут строго друг за другом работать
